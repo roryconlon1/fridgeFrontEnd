4c7d9b9c8b2631bb30208c9450f3f5671307f88a
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2022-10-07 18:24:28 +0100

    we can get from local api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom';
 import MainContainer from './containers/MainContainer';
 
