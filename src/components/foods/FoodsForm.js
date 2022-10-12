22ffde9afbf9f3de6224406f51c8c629908a1732
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2022-10-12 11:03:01 +0100

    checkbox updated

--- src/components/foods/FoodsForm.js
+++ src/components/foods/FoodsForm.js
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 
-const FoodsForm = ({onCreate}) => {
+const FoodsForm = ({ onCreate }) => {
 
-    const [stateIngredient, setStateIngredient] = useState ({
+    const [stateIngredient, setStateIngredient] = useState({
         name: "",
         calories: 0,
-        footType: null,
-        pantryFood: "",
-        fridgeFood: ""
+        footType: "",
+        pantryFood: false,
+        fridgeFood: false
     })
 
     const handleChange = (event) => {
         let propertyName = event.target.name;
-        let copyIngredient = {...stateIngredient}
-        copyIngredient[propertyName] = event.target.value;
+        let copyIngredient = { ...stateIngredient }
+        copyIngredient[propertyName] = (event.target.type === "checkbox") ? event.target.checked : event.target.value;
         setStateIngredient(copyIngredient);
     }
 
@@ -22,14 +22,23 @@ const FoodsForm = ({onCreate}) => {
         onCreate(stateIngredient);
     }
 
-    return(
+    return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Name" name="name" onChange={handleChange} value={stateIngredient.name}/>
-                <input type="number" placeholder="Calories" name="calories" onChange={handleChange} value={parseInt(stateIngredient.calories)}/>
-                <input type="checkbox" id="PantryFood" placeholder="PantryFood" name="pantryFood" onChange={handleChange} value={stateIngredient.pantryFood}/>
+                <input type="text" placeholder="Name" name="name" onChange={handleChange} value={stateIngredient.name} />
+                <input type="number" placeholder="Calories" name="calories" onChange={handleChange} value={parseInt(stateIngredient.calories)} />
+                <label for="foods">Choose a Food Type:</label>
+                <select id="foods" name="foods">
+                    <option value="CARBOHYDRATE">Carbohydrate</option>
+                    <option value="PROTEIN">Protein</option>
+                    <option value="FAT">Fat</option>
+                    <option value="DAIRY">Dairy</option>
+                    <option value="FRUIT">Fruit</option>
+                    <option value="VEGETABLE">Vegetable</option>
+                </select>
+                <input type="checkbox" id="PantryFood" placeholder="PantryFood" name="pantryFood" onChange={handleChange} value={stateIngredient.pantryFood} />
                 <label for="PantryFood">PantryFood</label>
-                <input type="checkbox" id="FridgeFood" placeholder="FridgeFood" name="fridgeFood" onChange={handleChange} value={stateIngredient.fridgeFood}/>
+                <input type="checkbox" id="FridgeFood" placeholder="FridgeFood" name="fridgeFood" onChange={handleChange} value={stateIngredient.fridgeFood} />
                 <label for="FridgeFood">FridgeFood</label>
                 <button type="submit">Save</button>
             </form>
@@ -37,6 +46,8 @@ const FoodsForm = ({onCreate}) => {
     )
 }
 
+
 export default FoodsForm;
+
 
 
