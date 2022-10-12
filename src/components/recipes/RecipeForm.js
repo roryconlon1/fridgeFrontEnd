6d19b006a836fcf8a31325193b1ceba5af1e059a
Greig Kerr <greigkerr@virginmedia.com>
2022-10-12 11:49:10 +0100

    Recipe enum showing in state

--- src/components/recipes/RecipeForm.js
+++ src/components/recipes/RecipeForm.js
@@ -5,7 +5,7 @@ const RecipeForm = ({onCreate}) => {
 
     const [stateRecipe, setStateRecipe] = useState ({
         name: "",
-        recipeType: null,
+        recipeType: "",
         cookTime: 0,
         method: ""
     })
@@ -32,6 +32,15 @@ const RecipeForm = ({onCreate}) => {
             Hello
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Name" name="name" onChange={handleChange} value={stateRecipe.name}/>
+                <select id="recipe" name="recipeType" onChange={handleChange}>
+                    <option value="SOUP">SOUP</option>
+                    <option value="PASTA">PASTA</option>
+                    <option value="SALAD">SALAD</option>
+                    <option value="PIE">PIE</option>
+                    <option value="FAMILY">FAMILY</option>
+                    <option value="DIET">DIET</option>
+                    <option value="DESERT">DESERT</option>
+                </select>
                 <input type="number" placeholder="Cook Time" name="cookTime" onChange={handleChange} value={parseInt(stateRecipe.cookTime)}/>
                 <input type="text" placeholder="Method" name="method" onChange={handleChange} value={stateRecipe.method}/>
                 <button type="submit">Save</button>
@@ -43,3 +52,4 @@ const RecipeForm = ({onCreate}) => {
 
 
 export default RecipeForm;
+
