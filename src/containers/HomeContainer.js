2620d2ae3a0b88e1fa58f5e42313bb7aaaa0fb95
Greig Kerr <greigkerr@virginmedia.com>
2022-10-17 11:31:37 +0100

    Changed to main container

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -3,7 +3,49 @@ import React from "react";
 const HomeContainer = () => {
 
     return(
-        <p>Hello</p>
+        <div>
+        <body>
+            <header>
+                <h1>HELLO</h1>
+                <nav></nav>
+            </header>
+
+            <div id="hero-image">
+                <h2>IMAGE PLACEHOLDER</h2>
+                <a>IMAGE PLACEHOLDER</a>
+            </div>
+
+            <div id="features">
+                <ul>
+                    <li>FEATURE1</li>
+                    <li>FEATURE2</li>
+                    <li>FEATURE3</li>
+                </ul>
+            </div>
+
+            <div id="primary-content">
+                <article>FOOD BLOG</article>
+            </div>
+
+            <div id="secondary-content">
+                <article>RESTURANT STUFF</article>
+                <article>FOOD STUFF</article>
+            </div>
+
+            <div id="cta">
+            </div>
+
+            <footer>
+                <div id="footer-info"></div>
+                <div id="footer-links">
+                    <ul>FOOT LINK ONE</ul>
+                    <ul>FOOT LINK TWO</ul>
+                    <ul>FOOT LINK THREE</ul>
+                </div>
+            </footer>
+
+        </body>
+    </div>
     )
 }
 
