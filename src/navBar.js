5017347a8dafc025d80d8486baebe8f428607143
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2022-10-18 15:21:52 +0100

    navbar styling

--- src/navBar.js
+++ src/navBar.js
@@ -5,13 +5,13 @@ import {Menu} from "semantic-ui-react";
 const NavBar = ({logout}) => (
     <Menu secondary >
         {/* <ul className="navBar"> */}
-        <Menu.Item name="Home" >
+        <Menu.Item name="Home" className="link">
             <Link to="/" className="nav item">Home</Link>
         </Menu.Item>
-        <Menu.Item name="Ingredients" >
+        <Menu.Item name="Ingredients" className="link" >
             <Link to="/foods" className="nav item">Ingredients</Link>
         </Menu.Item>
-        <Menu.Item name="Recipes">
+        <Menu.Item name="Recipes" className="link">
             <Link to="/recipes" className="nav item">Recipes</Link>
         </Menu.Item>
         <Menu.Menu position="right" className="logout">
